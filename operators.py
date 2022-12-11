4a30789bc797d4766ee38e281127c19d56b5825f
surya2208 <suryaforwin@gmail.com>
2022-12-11 18:43:52 +0530

    update commit

--- operators.py
+++ operators.py
@@ -6,3 +6,4 @@ print("a*b=",a*b)#20
 print("a/b=",a/b)#5.0 result of / is always going to be inform of float
 print("a%b=",a%b)#modulo operater - reminder is going to be zero, so answer is going to be zero
 print("a//b",a//b)#5, if arguments are int type then otput is also int type, if arguments are float type then output will be also float type
+
