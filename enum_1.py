bc4b0448e75c8f85c47e6cc683e1c3c095bcc3e1
surya2208 <suryaforwin@gmail.com>
2023-02-20 08:46:11 +0530

    renaming the file

---
+++ enum_1.py
@@ -0,0 +1,8 @@
+#Enumerate Function
+
+my_list = ['apples', 'pears', 'oranges', 'fruites']
+
+for x, element in enumerate(my_list):
+    print(x, element)
+    if x==1:
+        print('Pears humko accha nhi lagta hai')
